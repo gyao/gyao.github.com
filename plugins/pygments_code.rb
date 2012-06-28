e1db62f279e38b23607c6f6881db9165dce31c6e
gyao <gyao.reg@gmail.com>
2012-06-29 00:54:13 +0800

    finish octopress 101

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -11,7 +11,7 @@ module HighlightCode
     lang = 'objc' if lang == 'm'
     lang = 'perl' if lang == 'pl'
     lang = 'yaml' if lang == 'yml'
-    str = pygments(str, lang).match(/<pre>(.+)<\/pre>/m)[1].to_s.gsub(/ *$/, '') #strip out divs <div class="highlight">
+    str = pygments(str, lang).force_encoding("UTF-8").match(/<pre>(.+)<\/pre>/m)[1].to_s.gsub(/ *$/, '') #strip out divs <div class="highlight">
     tableize_code(str, lang)
   end
 
---
+++ source/_posts/2012-06-28-setup-rails-environment-on-mac-lion.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "Setup rails environment on Mac Lion"
+date: 2012-06-28 16:25
+comments: true
+categories: 
+---
